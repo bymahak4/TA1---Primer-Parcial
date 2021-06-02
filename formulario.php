ab1844aec611780e08307d24c3278ef4e806fe5c
Diego Rodriguez <rodriguezdiego.uy@gmail.com>
2021-06-01 22:06:31 -0300

    V.1.1 Primera Parte del Parcial

--- formulario.php
+++ formulario.php
@@ -1,2 +1,12 @@
 <?php
     
+        function mostrarPersona(){
+        echo "Nombre: {$_POST['nombre']} <br />";
+        echo "Apellido: {$_POST['apellido']} <br />";
+        echo "Telefono: {$_POST['telefono']} <br />"; 
+        echo "Fecha de nacimiento: {$_POST['date']} <br />";
+        echo "Edad: {$_POST['edad']} <br />";
+        echo "Correo: {$_POST['email']}";
+    }
+
+    mostrarPersona();
